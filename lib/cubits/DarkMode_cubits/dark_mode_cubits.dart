333e0973859af81e04f4abffd43d49c9f30573c0
momosarhan65 <momosarhan65@gmail.com>
2024-02-10 12:15:46 +0200

    adding dark mode cubit

---
+++ lib/cubits/DarkMode_cubits/dark_mode_cubits.dart
@@ -0,0 +1,14 @@
+import 'dart:developer';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:graduate/cubits/DarkMode_cubits/dark_mode_state.dart';
+
+class ModeStateCubit extends Cubit<ModeState> {
+  ModeStateCubit() : super(LightModeState());
+  bool _mode = false;
+  bool get mode => _mode;
+  set mode(bool value) {
+    _mode = value;
+    emit(_mode ? DarkModeState() : LightModeState());
+  }
+}
