a8d3cd281dd642057c730f24be4997fc40236b70
momosarhan65 <momosarhan65@gmail.com>
2024-02-10 12:15:07 +0200

    adding chat service

---
+++ lib/services/chat_services.dart
@@ -0,0 +1,62 @@
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:graduate/models/message_model.dart';
+import 'package:graduate/models/user_model.dart';
+
+class ChatServices extends ChangeNotifier {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
+
+  // Send message
+  Future<void> sendMessage(String reciverId, String message) async {
+    // get current user info
+    final String currentUserId = _firebaseAuth.currentUser!.uid;
+    final String currentUserEmail = _firebaseAuth.currentUser!.email.toString();
+    final Timestamp timestamp = Timestamp.now();
+
+    MessageModel newMessage = MessageModel(
+        message: message,
+        reciverId: reciverId,
+        senderId: currentUserId,
+        timestamp: timestamp);
+
+    List<String> ids = [currentUserId, reciverId];
+    ids.sort();
+    String chatRoomId = ids.join("_");
+    await _firebaseFirestore
+        .collection(
+          'chat_rooms',
+        )
+        .doc(chatRoomId)
+        .collection('messages')
+        .add(newMessage.toMap());
+  }
+
+  Stream<QuerySnapshot> getMessage(
+      {required String userId, required String otherUserId}) {
+    List<String> ids = [userId, otherUserId];
+    ids.sort();
+    String chatRoomId = ids.join("_");
+    return _firebaseFirestore
+        .collection('chat_rooms')
+        .doc(chatRoomId)
+        .collection('messages')
+        .orderBy('timestamp', descending: true)
+        .snapshots();
+  }
+
+  Stream<QuerySnapshot> getFriends({required UserModel user}) {
+    String collection = 'users';
+    log(user.email);
+    return FirebaseFirestore.instance
+        .collection(collection)
+        .where('userType', isEqualTo: user.userType)
+        .where('level', isEqualTo: user.level)
+        .where('department', isEqualTo: user.department)
+        // .where('email', isNotEqualTo: user.email)
+        .snapshots();
+  }
+}
