f6457f4293d937103c2204c79582ca7cf9759268
momosarhan65 <momosarhan65@gmail.com>
2024-02-11 13:43:58 +0200

    rebuild home Page

---
+++ lib/Pages/AuthGate.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:graduate/Pages/MainPage.dart';
+import 'package:graduate/Pages/LoginPage.dart';
+import 'package:graduate/cubits/Login_cubits/login_cubits.dart';
+import 'package:graduate/cubits/Login_cubits/login_cubits_state.dart';
+import 'package:graduate/cubits/Navigation_cubits/navigation_cubit.dart';
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<LoginStateCubit, SignUpState?>(
+      builder: (context, user) {
+        if (user is NotLoginYet) {
+          BlocProvider.of<NavigationCubit>(context).navigationToLogin();
+        } else {
+          BlocProvider.of<NavigationCubit>(context).navigationToHome();
+        }
+        return BlocBuilder<NavigationCubit, AppPage>(
+            builder: (context, appPage) {
+          if (appPage == AppPage.home) {
+            return MainPage();
+          } else {
+            return LoginPage();
+          }
+        });
+      },
+    );
+  }
+}
