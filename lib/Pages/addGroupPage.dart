84dacd72f1d2273e92c8b31f0cba2f008730bf69
momosarhan65 <momosarhan65@gmail.com>
2024-03-12 16:51:09 +0200

    adding frined and members list

--- lib/Pages/addGroupPage.dart
+++ lib/Pages/addGroupPage.dart
@@ -1,4 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:graduate/cubits/DarkMode_cubits/dark_mode_cubits.dart';
+import 'package:graduate/cubits/Login_cubits/login_cubits.dart';
+import 'package:graduate/models/user_model.dart';
+import 'package:graduate/services/chat_services.dart';
+import 'package:graduate/services/chooseIcons_services.dart';
 
 class AddGroupPage extends StatefulWidget {
   const AddGroupPage({super.key});
@@ -10,6 +17,8 @@ class AddGroupPage extends StatefulWidget {
 
 class _AddGroupPageState extends State<AddGroupPage> {
   final TextEditingController _groupNameController = TextEditingController();
+  final _chooseIconService = ChooseIconService();
+  List<UserModel> selectedUsers = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,14 +32,79 @@ class _AddGroupPageState extends State<AddGroupPage> {
           ),
           title: TextField(
             controller: _groupNameController,
-            decoration:const InputDecoration(hintText: 'Group Name',),
+            decoration: const InputDecoration(
+              hintText: 'Group Name',
+            ),
           ),
-        )
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        const Text(
+          'Member',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+        ),
+        const Text(
+          'Friends',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+        ),
+        Expanded(child: _buildUserList())
       ]),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: const Icon(Icons.arrow_forward),
       ),
+    );
+  }
+
+  Widget _buildUserItem(UserModel friend) {
+    final isSelected = selectedUsers.contains(friend);
+    return ListTile(
+      leading: CircleAvatar(
+          child: FutureBuilder(
+        future: _chooseIconService.getImageByUid(uid: friend.uid!),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          }
+          if (snapshot.connectionState == ConnectionState.done &&
+              snapshot.hasData &&
+              snapshot.data != null) {
+            return CircleAvatar(
+              backgroundImage: NetworkImage(snapshot.data!),
+            );
+          } else {
+            return const Icon(Icons.person);
+          }
+        },
+      )),
+      title: Text(
+        friend.getFullName(),
+        style: TextStyle(
+            color: BlocProvider.of<ModeStateCubit>(context).mode
+                ? Colors.white
+                : Colors.black),
+      ),
+    );
+  }
+
+  Widget _buildUserList() {
+    return StreamBuilder<QuerySnapshot>(
+      stream: ChatServices().getFriends(
+          user: BlocProvider.of<LoginStateCubit>(context).userModel!),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return const Center(child: Text('Error'));
+        }
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+        return ListView(
+          children: snapshot.data!.docs
+              .map<Widget>((doc) => _buildUserItem(UserModel.fromDocs(doc)))
+              .toList(),
+        );
+      },
     );
   }
 }
