a964a230071beac857df8ec07a51555296d5b1a8
momosarhan65 <momosarhan65@gmail.com>
2024-04-20 00:54:01 +0200

    initial firebase messeging

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:graduate/Pages/AddPostPage.dart';
@@ -21,6 +22,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await FirebaseMessegingApi().initNotification();
+  //await FirebaseMessaging.instance.setAutoInitEnabled(true);
   runApp(const MyApp());
 }
 
--- lib/services/firebaseMesseging_apI.dart
+++ lib/services/firebaseMesseging_apI.dart
@@ -4,13 +4,22 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 
 class FirebaseMessegingApi {
   final _firebaseMessage = FirebaseMessaging.instance;
+
   Future<void> initNotification() async {
     await _firebaseMessage.requestPermission(
+      provisional: true,
       alert: true,
       sound: true,
       badge: true,
     );
     final fCMToken = await _firebaseMessage.getToken();
-    log(fCMToken!);
+    log("heeeer:${fCMToken}");
+    FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
   }
+}
+
+Future<void> handleBackgroundMessage(RemoteMessage message) async {
+  log('Title:${message.notification?.title}');
+  log('body:${message.notification?.body}');
+  log('payload:${message.data}');
 }
